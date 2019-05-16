b3c9b398cb6bff751ceb24ceca446d76ec4551f0
Alexander G. Yang <spark8402@rogers.com>
2019-05-16 01:00:01 -0400

    assignment done

---
+++ exercise.py
@@ -0,0 +1,36 @@
+class Task:
+
+    def __init__(self,description, due_date):
+        self.description = description
+        self.due_date = due_date
+    
+    def __str__(self):
+        return "{},{}".format(self.description, self.due_date)
+
+class TodoList:
+
+    def __init__(self, tasks):
+        self.tasks = tasks
+    
+    task_list = []
+    
+    def __repr__(self):
+        return "{}".format(self.task_list)
+    
+    @classmethod
+    def add_task(cls, todo):
+        cls.task_list.append(todo)
+
+judo = Task("smash them up", "next week")
+coding = Task("code like crazy", "tomorrow")
+watch_Raptors = Task("go Raptors go!", "tonight!")
+
+TodoList.add_task(judo)
+print(TodoList.task_list)
+TodoList.add_task(coding)
+print(TodoList.task_list)
+TodoList.add_task(watch_Raptors)
+print(TodoList.task_list)
+print(TodoList.task_list[0])
+print(TodoList.task_list[1])
+print(TodoList.task_list[2])
